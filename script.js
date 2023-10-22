3323a00246e933a5fcfe8d28fdcf59b61969cc32
David Mikulic <david.mikulic44@gmail.com>
2023-10-22 15:15:18 +0200

    commit before refactoring

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#next').hover(
+    function() {
+        $(this).css('background-image', 'url(assets/arrow-blue-right.png');
+        $(this).css('border', '1px solid #134880');
+        $(this).css('cursor', 'pointer');
+    },
+
+    function() {
+        $(this).css('background-image', 'url(assets/arrow-gray-right.png');
+        $(this).css('border', '1px solid #47494C');
+    }
+    )
+
+    $('#previous').hover(
+        function(){
+            $(this).css('background-image', 'url(assets/arrow-blue-left.png');
+            $(this).css('border', '1px solid #134880');
+            $(this).css('cursor', 'pointer');
+        },
+        function(){
+            $(this).css('background-image', 'url(assets/arrow-gray-left.png');
+            $(this).css('border', '1px solid #47494C');
+        }
+    )
+
+});
+
