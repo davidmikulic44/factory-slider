f57add2f7e77b8c64501f422c694455b04f752da
davidmikulic44 <david.mikulic44@gmail.com>
2023-10-23 21:05:20 +0200

    fixed button css

--- src/script.js
+++ src/script.js
@@ -4,18 +4,17 @@ $(document).ready(function() {
     var $topSlider = $('.top-slider');
     var $bottomSlider = $('.bottom-slider');
 
+
     function moveImagesRight($slider) {
         var $lastImage = $slider.children().last();
         var imageWidth = $lastImage.width();
 
         $lastImage.css({ 'opacity': 1, 'width': imageWidth })
-            .animate({ 'opacity': 0, 'width': 0 }, animationSpeed, function() {
+            .animate({ 'opacity': 0, 'width': 0 }, function() {
 
                 $slider.prepend($lastImage);
                 $lastImage.css({ 'opacity': 1, 'width': imageWidth });
-                $slider.css({ 'left': -imageWidth }).animate({ 'left': 0 }, animationSpeed);
-
-                $('#next').css({ 'pointer-events': 'auto' });
+                $slider.css({ 'left': -imageWidth }).animate({ 'left': 0 });
             });
     }
 
@@ -24,52 +23,48 @@ $(document).ready(function() {
         var $firstImage = $slider.children().first();
         var imageWidth = $firstImage.width();
 
-        $slider.css({ 'left': 0 }).animate({ 'left': -imageWidth }, animationSpeed, function() {
+        $slider.css({ 'left': 0 }).animate({ 'left': -imageWidth }, 0, function() {
             $slider.append($firstImage);
-            $firstImage.css({ 'opacity': 0, 'width': 0 }).animate({ 'opacity': 1, 'width': imageWidth });
+            $firstImage.css({ 'opacity': 0, 'width': 0 })
+            .animate({ 'opacity': 1, 'width': imageWidth });
 
             $slider.css({ 'left': '0px' });
-            $('#previous').css({ 'pointer-events': 'auto' });
         });
     }
 
 
     $('#previous').on('click', function() {
-        $('#previous').css('pointer-events', 'none');
         moveImagesLeft($topSlider);
-        $('#previous').css('pointer-events', 'none');
         moveImagesLeft($bottomSlider);
     });
 
 
     $('#next').on('click', function() {
-        $('#next').css('pointer-events', 'none');
         moveImagesRight($topSlider);
-        $('#next').css('pointer-events', 'none');
         moveImagesRight($bottomSlider);
     });
 
     $('#next').hover(
         function() {
-            $(this).css('background-image', 'url(assets/arrow-blue-right.png');
+            $(this).css('background-image', 'url(../assets/arrow-blue-right.png');
             $(this).css('border', '1px solid var(--foreground-color-primary)');
             $(this).css('cursor', 'pointer');
         },
 
         function() {
-            $(this).css('background-image', 'url(assets/arrow-gray-right.png');
+            $(this).css('background-image', 'url(../assets/arrow-gray-right.png');
             $(this).css('border', '1px solid var(--background-color-secondary)');
         }
     );
 
     $('#previous').hover(
         function() {
-            $(this).css('background-image', 'url(assets/arrow-blue-left.png');
+            $(this).css('background-image', 'url(../assets/arrow-blue-left.png');
             $(this).css('border', '1px solid var(--foreground-color-primary)');
             $(this).css('cursor', 'pointer');
         },
         function() {
-            $(this).css('background-image', 'url(assets/arrow-gray-left.png');
+            $(this).css('background-image', 'url(../assets/arrow-gray-left.png');
             $(this).css('border', '1px solid var(--background-color-secondary)');
         }
     );
